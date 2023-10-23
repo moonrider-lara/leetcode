77bb9f2fe49cd95190461b41abb72569c80e3fe7
Hera Jang <97158923+codingfairy-lara@users.noreply.github.com>
2023-10-23 16:06:59 +0900

    Time: 1 ms (82.88%), Space: 40.2 MB (72.61%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,37 +1,18 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int idx = 0;
         
-        StringBuilder result = new StringBuilder(); 
-        int len = strs.length;
-        int s = 0; // the shortest str index
-        int shortest = strs[0].length(); // the shortest str length
-        
-        if(len == 1)
-            return strs[0];
-        
-        for (int i = 0; i < len; i++) {
-            if (shortest > strs[i].length()){
-                shortest = strs[i].length();
-                s = i;
+        while(idx < s1.length() && idx < s2.length()){
+            if(s1.charAt(idx) == s2.charAt(idx)){
+                idx++;
+            } else {
+                break;
             }
         }
         
-        int charNum = 0;
-        while (charNum < shortest) {
-
-            for (int i = 0; i< len; i++) {
-                char c = strs[s].charAt(charNum);
-                if (strs[i].charAt(charNum) == c && i == len-1) {
-                    result.append(c);
-                } else if (strs[i].charAt(charNum) == c) {
-                    continue;
-                }else {
-                    return result.toString();
-                }
-            }
-            charNum++;
-        }
-        
-        return result.toString();
+        return s1.substring(0, idx);
     }
 }
