8c81d85099409f19837d255ea7e2d87e45632587
Hera Jang <97158923+codingfairy-lara@users.noreply.github.com>
2023-10-31 16:44:24 +0900

    Time: 1 ms (98.78%), Space: 41 MB (7.87%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean isValid(String s) {
+       
         Deque<Character> st = new ArrayDeque<>();
         for (char ch: s.toCharArray()) {
             if (st.peek() == complement(ch)) st.pop();
             else st.push(ch);
         }
+        
         return st.isEmpty();
     }
 
